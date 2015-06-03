e6b4284ed88dba6db45d6411f15ecefe6d57ac69
Eivin Landa <landa@drivdigital.no>
2015-06-03 17:05:54 +0200

    First working draft of the cache plugin

--- class/class.nginx-fpc-interface.php
+++ class/class.nginx-fpc-interface.php
@@ -1,20 +1,34 @@
 <?php
 
 class Nginx_Fpc_Interface {
+
+  // Cache choices for the cache type setting
+  static $cache_choices = [
+    0 => 'none',
+    1 => 'static',
+    2 => 'all'
+  ];
+
+  /**
+   * Setup
+   */
   static function setup() {
-    add_action( 'post_submitbox_start', 'Nginx_Fpc_Interface::sync_checkbox' );
-    add_action( 'save_post', 'Nginx_Fpc_Interface::sync_checkbox_submit', 100, 1 );
+    // Add a checkbox for every post
+    add_action( 'post_submitbox_start', 'Nginx_Fpc_Interface::cache_checkbox' );
+    add_action( 'save_post', 'Nginx_Fpc_Interface::cache_checkbox_submit', 100, 1 );
 
-
+    // Add a settings page for cache settings
     add_action( 'admin_menu', 'Nginx_Fpc_Interface::add_admin_menu' );
     add_action( 'admin_init', 'Nginx_Fpc_Interface::settings_init' );
   }
 
-
-  static function sync_checkbox() {
+  /**
+   * Sync checkbox
+   */
+  static function cache_checkbox() {
     $post = get_post();
-    if ( !in_array( $post->post_type, ['product', 'post', 'page'] ) ) {
-      // Return early if this is not a product
+    // Only for allowed post-types
+    if ( !in_array( $post->post_type, Nginx_Fullpage_Cache::cacheable_post_types() ) ) {
       return;
     }
 
@@ -27,29 +41,34 @@ class Nginx_Fpc_Interface {
   <?php
   }
 
-  static function sync_checkbox_submit( $post_id ) {
+  static function cache_checkbox_submit( $post_id ) {
     $post = get_post( $post_id );
-    // Only for products
-    if ( in_array( $post->post_type, ['product', 'post', 'page'] ) ) {
+    // Only for allowed post-types
+    if ( !in_array( $post->post_type, Nginx_Fullpage_Cache::cacheable_post_types() ) ) {
+      return;
+    }
 
-      // Make sure the request was not triggered from the quick edit form
-      if ( !isset( $_POST['nginx-fpc-confirm'] ) ) {
-        return;
-      }
+    // Make sure the request was not triggered from the quick edit form
+    if ( !isset( $_POST['nginx-fpc-confirm'] ) ) {
+      return;
+    }
 
-      $sync = isset( $_POST['nginx-fpc'] );
-      if ( FALSE !== get_post_meta( $post_id, '_nginx_fpc', 1 ) ) {
-        update_post_meta( $post_id, '_nginx_fpc', $sync ? 'on' : 'off' );
-      }
-      else {
-        add_post_meta( $post_id, '_nginx_fpc', $sync ? 'on' : 'off' );
-      }
+    // Was the checkbox checked?
+    $sync = isset( $_POST['nginx-fpc'] );
+
+    // Set the post cache meta-setting
+    if ( FALSE !== get_post_meta( $post_id, '_nginx_fpc', 1 ) ) {
+      update_post_meta( $post_id, '_nginx_fpc', $sync ? 'on' : 'off' );
+    }
+    else {
+      add_post_meta( $post_id, '_nginx_fpc', $sync ? 'on' : 'off' );
     }
   }
 
 
-
-
+  /**
+   * Add an admin menu item (under "Settings")
+   */
   static function add_admin_menu(  ) {
     add_options_page(
       // Page <title>
@@ -65,10 +84,14 @@ class Nginx_Fpc_Interface {
     );
   }
 
-
+  /**
+   * Settings init
+   * Initialise settings and run submit handling
+   */
   static function settings_init(  ) {
     register_setting( 'nginx-fpc-options-page', 'fpc_settings' );
 
+    // Add a section for the fpc settings
     add_settings_section(
       'nginx-fpc-section',
       __( 'Cache settings', 'nginx-fpc' ),
@@ -77,13 +100,21 @@ class Nginx_Fpc_Interface {
     );
 
     $settings = [
-      'text_api_key'        => __( 'RaskeSider.no API key', 'nginx-fpc' ),
-      'text_api_secret'     => __( 'RaskeSider.no API secret', 'nginx-fpc' ),
-      'checkbox_devmode'    => __( 'RaskeSider.no Developer mode', 'nginx-fpc' ),
-      // 'select_field_3'   => __( 'Settings field description', 'nginx-fpc' ),
+      'text_api_key'        => __( 'BitSled.com API key', 'nginx-fpc' ),
+      // API Secret is not yet implemented
+      // 'text_api_secret'     => __( 'BitSled.com API secret', 'nginx-fpc' ),
+      // Developer mode (pagespeed) [on|off] is not yet implemented
+      // 'checkbox_devmode'    => __( 'BitSled.com Developer mode', 'nginx-fpc' ),
+      // Button for flushing tengine cache
+      // 'button_flush_cache'    => __( 'Flush cache', 'nginx-fpc' ),
+      'select_cache_status'   => __( 'Cache type', 'nginx-fpc' ),
       'textarea_post_types' => __( 'Post types to enable caching for', 'nginx-fpc' ),
     ];
 
+    // Filter cache status
+    add_filter( 'nginx_fpc_field_default', 'Nginx_Fpc_Interface::filter_default_cache_status', 10 , 2 );
+
+    // Run the settings through the add_settings_field function
     foreach ( $settings as $key => $value ) {
       $params = [
         "fpc_$key",
@@ -95,10 +126,42 @@ class Nginx_Fpc_Interface {
       call_user_func_array( 'add_settings_field', $params );
     }
 
-
+    // Submit handling
+    if ( isset( $_POST['fpc_settings'], $_POST['fpc_settings']['fpc_cache_status'] ) ) {
+      $choice = $_POST['fpc_settings']['fpc_cache_status'];
+      if ( -1 != $choice && self::get_cache_status() != $choice ) {
+        $choices = self::$cache_choices;
+        $result = self::set_cache_status( $choice );
+        // Add a message
+        if ( 'success' != @$result->status )
+          self::add_message( 'Could not update the nginx cache setting. Connection error!', 'error' );
+        else
+          self::add_message( 'Cache config set to <strong>'. $choices[$choice] .'</strong>' );
+      }
+    }
   }
 
-  function __callStatic( $name, $args ) {
+  /**
+   * Add message
+   * @param string $message Message to be displayed
+   * @param string $type    The type of message, "updated" or "error"
+   */
+  static function add_message( $message, $type = 'updated' ) {
+    $messages = get_option( 'nginx_fpc_messages', [] );
+    if ( !isset( $messages[$type] ) )
+      $messages[$type] = [];
+    $messages[$type][] = $message;
+    update_option( 'nginx_fpc_messages', $messages );
+  }
+
+  /**
+   * Call static (Magic method)
+   * Used for render_ method calls
+   * @param  string $name Called method name
+   * @param  array  $args Arguments used
+   * @return mixed        False for unmatched method names
+   */
+  static function __callStatic( $name, $args ) {
     static $options = FALSE;
     if ( 0 !== strpos( $name, 'render_' ) ) {
       return FALSE;
@@ -107,52 +170,92 @@ class Nginx_Fpc_Interface {
     if ( !preg_match( '/^render_([^_]*)_(.*)$/', $name, $matches ) ) {
       return FALSE;
     }
+    // Assign parts to variables
+    list( $name, $type, $id ) = $matches;
 
+    // Get cache options
     if ( !$options ) {
       $options = get_option( 'fpc_settings' );
     }
 
-    list( $name, $type, $id ) = $matches;
+    // Set defaults
     $format = '';
-    $default = $options['fpc_'. $type .'_'. $id];
+    $default = @$options['fpc_'. $id];
+
+    // Parse different field types
     if ( 'text' == $type ) {
-      $format  = "<input type='text' name='fpc_settings[%s]' value='%s'>";
+      $format  = "<input type=text size=25 name='fpc_settings[%s]' value='%s'>";
     }
     elseif ( 'checkbox' == $type ) {
       $format  = "<input type='checkbox' name='fpc_settings[%s]' %s value='1' id='%1\$s'> <label for='%1\$s'>On</label>";
-      $default = checked( $options['fpc_checkbox_'. $id], 1, 0 );
+      $default = checked( @$options['fpc_'. $id], 1, 0 );
     }
     elseif ( 'select' == $type ) {
-      $format  = "<select name='fpc_settings[%s]' multiple=multiple>%s</select>";
-      // selected( $options['fpc_select_field_3'], 2 );
-      $default = '<option value=1>Option 1</option><option value=2>Option 2</option>';
+      // Any select field should be parsed with the field_default filter
+      $format  = "<select name='fpc_settings[%s]'>%s</select>";
+      $default = '<option>- empty -</option>';
     }
     elseif ( 'textarea' == $type ) {
       $format = "<textarea cols='40' rows='5' name='fpc_settings[%s]'>%s</textarea>";
     }
-    printf( $format, 'fpc_'. $type .'_'. $id, $default );
+    // Apply filters
+    $format  = apply_filters( 'nginx_fpc_field_format',  $format,  $id, $type );
+    $default = apply_filters( 'nginx_fpc_field_default', $default, $id, $type );
+    printf( $format, 'fpc_'. $id, $default );
+
+    return TRUE;
   }
 
+  /**
+   * Filter {$default} cache_status
+   * @param  string $default HTML for the <option> tags
+   * @param  string $id      id handle used to identify the correct field
+   * @return                 HTML for the <option> tags
+   */
+  static function filter_default_cache_status( $default, $id ) {
+    // Only process cache status
+    if ( 'cache_status' !== $id )
+      return $default;
 
-  static function settings_section_callback(  ) {
-    // echo __( '', 'nginx-fpc' );
-    // Any messages goes here?
+    if ( !self::get_api_key() )
+      return '<option value=-1>- missing api key -</option>';
+
+    // Get available choices and selected status
+    $choices = self::$cache_choices;
+    $selected = self::get_cache_status();
+    if ( -1 !== $selected ) {
+      $default = '';
+      foreach ( $choices as $key => $choice ) {
+        $default .= sprintf( '<option value=%s%s>%s</option>', $key, selected( $selected, $key, 0 ), $choice );
+      }
+    }
+    else {
+      // Could not fetch the cache status.
+      // Probably because of an API error
+      $default = '<option value=-1>- api error -</option>';
+    }
+    return $default;
   }
 
+  /**
+   * Settings section callback
+   * Render html for the section head
+   */
+  static function settings_section_callback() {
+    // Empty
+  }
 
+  /**
+   * Options page callback
+   */
   static function options_page(  ) {
+    $messages = get_option( 'nginx_fpc_messages', [] );
+    update_option( 'nginx_fpc_messages', [] );
 
+    foreach ($messages as $class_attr => $message ) {
+      printf( '<div class="%s settings-error">%s</div>', $class_attr, '<p>'. implode( '</p><p>', $message ) .'</p>' );
+    }
     ?>
-    <div class="error settings-error">
-      <p><strong>Connection to raskesider.no was refused</strong></p>
-    </div>
-    <div class="error settings-error">
-      <p><strong>You must enter API credentials to toggle the developer mode</strong></p>
-    </div>
-    <div class="updated settings-error">
-      <p><strong>Developer mode "On" has been requested</strong></p>
-      <p><strong>Developer mode "Off" has been requested</strong></p>
-    </div>
     <form action='options.php' method='post'>
 
       <h2>Nginx Fullpage Cache</h2>
@@ -166,6 +269,98 @@ class Nginx_Fpc_Interface {
     </form>
     <?php
 
+  }
+
+  /**
+   * Get the api key
+   * @return string BitSled API key
+   */
+  static function get_api_key() {
+    // Get the plugin options
+    $options = get_option( 'fpc_settings' );
+    return $options['fpc_api_key'];
+  }
+  /**
+   * Api Endpoint
+   * @param  string $which Which endpoint to get
+   * @return mixed         (string) Endpoint on success. Otherwise FALSE
+   */
+  static function api_endpoint( $which = 'nginx' ) {
+    $vhost_id = self::get_vhost_number();
+    if ( -1 === $vhost_id )
+      return FALSE;
+
+    // Choose which endpoint to use
+    if ( 'nginx' == $which )
+      $endpoint = sprintf( '/api/vhost/%s/feature/nginx_simplecache', $vhost_id );
+    else
+      return FALSE;
+
+    // Get the API-key
+    $key = self::get_api_key();
+    // API requires a KEY, return false if no key is set
+    if ( !$key )
+      return FALSE;
+    $url = 'https://raskesider.no';
+
+    return "{$url}{$endpoint}?token={$key}";
+  }
+
+  /**
+   * Get the cache status from raskesider
+   * @return int The status on success, -1 on failure
+   */
+  static function get_cache_status() {
+    $endpoint = self::api_endpoint();
+    if ( !$endpoint )
+      return -1;
+    $request = wp_remote_get( $endpoint );
+    $value = -1;
+    // Validate response
+    if ( $request && !is_wp_error( $request ) ) {
+      if ( 200 == $request['response']['code'] ) {
+        $json = json_decode( $request['body'] );
+        $value = $json->data->value;
+      }
+    }
+
+    return $value;
+  }
+  /**
+   * Set the cache status
+   * @param int $value The value to set. Valid choices are: [0|none, 1|static files, 2|all]
+   */
+  static function set_cache_status( $value ) {
+    if ( !in_array( $value, [0, 1, 2] ) )
+      return FALSE;
+    $vhost_id = self::get_vhost_number();
+    $request = wp_remote_post( self::api_endpoint(), [
+      'body' => ['value' => $value]
+    ] );
+
+    // Validate response
+    if ( $request && !is_wp_error( $request ) ) {
+      if ( 200 == $request['response']['code'] ) {
+        $json = json_decode( $request['body'] );
+      }
+    }
+
+    return $json;
+  }
+  /**
+   * Get vhost number
+   * @return int vhost number
+   */
+  static function get_vhost_number() {
+    $reg = '/^\/'
+    .'[^\/]*' .'\/'       // Usually /kunder/
+    .'[^\/]*_(\d+)' .'\/' // Webhost number
+    .'[^\/]*_(\d+)' .'\/' // Vhost number
+    .'/';// End
+    if ( !preg_match( $reg, getcwd(), $matches ) )
+      return -1;
+
+    return $matches[2];
   }
 
 }
--- nginx-fullpage-cache.php
+++ nginx-fullpage-cache.php
@@ -1,7 +1,14 @@
 <?php
 class Nginx_Fullpage_Cache {
+  static $post_types = [];
+  /**
+   * Setup
+   */
   static function setup() {
+    // Include the interface
     require_once 'class/class.nginx-fpc-interface.php';
+
+    // Attach the method for setting cache headers
     add_filter( 'posts_results', array( 'Nginx_Fullpage_Cache', 'set_headers' ) );
   }
 
@@ -10,32 +17,57 @@ class Nginx_Fullpage_Cache {
    * Determine and set the type of headers to be used
    */
   static function set_headers( $posts ) {
-    if ( is_admin() ) {
+    // No cache for logged in users
+    if ( is_user_logged_in() ) {
+      setcookie( "no_cache", 1, time() + 3600, COOKIEPATH, COOKIE_DOMAIN );
+    }
+    // Set no-cache for all admin pages
+    if ( is_admin() || is_user_logged_in() ) {
       nocache_headers();
       return $posts;
     }
     // Only trigger this function once.
     remove_filter( 'posts_results', 'nginx_cache_headers' );
 
-    // Cache all archives
-    if ( !is_archive() ) {
-      // Loop through selected posts
+
+    if ( ( is_singular() || is_page() ) && in_array( get_post_type(), self::cacheable_post_types() ) ) {
+
+      // Check if cache is disabled for the post
       foreach ( $posts as $post ) {
-        // Check if cache is disabled for the post
         if ( 'off' === get_post_meta( $post->ID, '_nginx_fpc', true ) ) {
-          // Disable caching for this page
+          // Disable caching for this page / post
           nocache_headers();
           return $posts;
         }
       }
+      // Make sure the post type can be cached
+      self::$post_types[] = get_post_type();
+      self::cache_headers();
     }
-    if ( is_singular() || is_page() || is_archive() ) {
+    elseif ( is_archive() && self::can_cache_archive( $posts ) ) {
+      // Make sure the archive has only cachable posts
       self::cache_headers();
     }
     else {
+      // Default to no-cache headers
       self::no_cache_headers();
     }
     return $posts;
+  }
+
+  /**
+   * Can cache archive
+   * @param  array $posts Posts in the archive
+   * @return boolean      Return true if all posts are cacheable
+   */
+  static function can_cache_archive( $posts ) {
+    foreach ( $posts as $post ) {
+      if ( !in_array( $post->post_type, self::cacheable_post_types() ) )
+        return FALSE;
+      elseif ( !in_array( $post->post_type, self::$post_types ) )
+        self::$post_types[] = $post->post_type;
+    }
+    return TRUE;
   }
 
   /**
@@ -48,6 +80,7 @@ class Nginx_Fullpage_Cache {
     header_remove( 'Expires' );
     header( 'Pragma: public' );
     header( 'Cache-Plugin: active' );
+    header( 'Cache-Post-Type: '. implode( ',', self::$post_types ) );
   }
 
   /**
@@ -55,11 +88,32 @@ class Nginx_Fullpage_Cache {
    * Print headers that prevent caching
    */
   static function no_cache_headers() {
-    // Set no-cache headers
     header( 'Cache-Control:max-age=0, no-cache' );
     header( 'Pragma: no-cache' );
     header( 'Cache-Plugin: pending' );
   }
+
+  /**
+   * Cacheable post types
+   * @return array A list of cacheable post types
+   */
+  static function cacheable_post_types() {
+    // Set default cacheable post types
+    $post_types = ['post', 'page', 'product'];
+    // Check option for values
+    $options = get_option( 'fpc_settings' );
+    if ( isset( $options['fpc_post_types'] ) ) {
+      // Parse comma separated list into an array
+      $post_types = explode( ',', $options['fpc_post_types'] );
+      foreach ( $post_types as &$post_type ) {
+        $post_type = trim( $post_type );
+      }
+      // Filter empty values
+      $post_types = array_filter( $post_types );
+    }
+    return $post_types;
+  }
 }
 
+// Start the plugin
 Nginx_Fullpage_Cache::setup();
