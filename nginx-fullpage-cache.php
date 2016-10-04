de35a66030af39f81efdfa94889afc30b82e5c25
Erlend Eide <erlend@raskesider.no>
2016-10-04 17:28:02 +0200

    Reduced browser cache to 10 minutes and added explicit 10 minute expiry for downstream caches and proxies.

--- nginx-fullpage-cache.php
+++ nginx-fullpage-cache.php
@@ -78,9 +78,11 @@ class Nginx_Fullpage_Cache {
     header_remove( 'Cache-Control' );
     header_remove( 'Pragma' );
     header_remove( 'Expires' );
-    // Allow browser to cache content for 1 hour
-    header( 'Cache-Control: max-age=3600,public' );
+    // Allow browser to cache content for 10 minutes
+    header( 'Cache-Control:max-age=600, public' );
     header( 'Pragma: public' );
+    // Expire in front-end caches and proxies after 10 minutes
+    header( 'Expires: '. gmdate('D, d M Y H:i:s', $_SERVER['REQUEST_TIME'] + 600) .' GMT');
     header( 'Cache-Plugin: active' );
     header( 'Cache-Post-Type: '. implode( ',', self::$post_types ) );
   }
